3043d0acd9dec2a746128ceb588968b5cca7bf00
Eder Garcia <eder000000@gmail.com>
2025-09-13 22:22:48 +0000

    CITASVIEJO-176 Subtask T-001-1: Maven Project Initialization

---
+++ src/main/java/com/geriatriccare/GeriatricHomeCareApplication.java
@@ -0,0 +1,11 @@
+package com.geriatriccare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeriatricHomeCareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeriatricHomeCareApplication.class, args);
+    }
+}
