84bfdacc5b25196a3320d7a735a934f4818b1214
Eder Garcia <eder000000@gmail.com>
2025-09-20 07:22:15 +0000

    GHCS-EPIC-000 System Infrastructure & Security | Test fixed

--- src/test/java/com/geriatriccare/GeriatricHomeCareApplicationTests.java
+++ src/test/java/com/geriatriccare/GeriatricHomeCareApplicationTests.java
@@ -3,11 +3,9 @@ package com.geriatriccare;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.yml")
 class GeriatricHomeCareApplicationTests {
 
     @Test
